804cc89fe4d5312f4117a1acf1943680d4383820
Chris <christian.hegi99@gmail.com>
2025-03-24 13:44:00 +0900

    plain setup

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hey There just a quick test</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "black",
+    borderWidth: 3,
+    borderColor: "black",
+    borderRadius: 5,
+    padding: 5,
   },
 });
